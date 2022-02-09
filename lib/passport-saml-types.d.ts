7b0de54f9de611183df2e3a077a11b171109f8f4
Guillaume Gautreau <guillaumeg@forestadmin.com>
2022-02-09 10:27:16 +0100

    chore: publish the prebuilt library

---
+++ lib/passport-saml-types.d.ts
@@ -0,0 +1,8 @@
+import * as passport from "passport";
+export interface AuthenticateOptions extends passport.AuthenticateOptions {
+    samlFallback?: "login-request" | "logout-request";
+    additionalParams?: Record<string, any>;
+}
+export interface AuthorizeOptions extends AuthenticateOptions {
+    samlFallback?: "login-request" | "logout-request";
+}
